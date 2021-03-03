6ee090433c51829237a5f9a9b274659fdf2174cf
Adrian Widlak <awidlak@adrianwii.pl>
2021-03-03 11:05:22 +0100

    Code review

--- main.js
+++ main.js
@@ -1,55 +1,58 @@
 const body = document.querySelector("body");
-const myUrl = 'http://api.tvmaze.com/schedule?country=';
+const apiUrl = 'http://api.tvmaze.com';
 const regex = /^((1[89])|2[0-4])/gi;
 const regexCat = /Adventure|Action/gi;
 
-(async function () {
+(async function() {
     try {
-        const datas = await getData(myUrl, 'CN', '2020-03-03');
-        getHour(datas);
-    } catch(e) {
+        const datas = await getData('CN', '2020-03-03');
+        filterSchedule(datas);
+    } catch (e) {
         console.error(e.message);
     }
-}) ();
+})();
 
-async function getData(url=myUrl, country='CN', date='2020-03-03') {
-    const apiUrl = `${url}${country}&date=${date}`;
-    const response = await fetch(apiUrl);
-    if(response.ok) {
+async function getData(country = 'CN', date = '2020-03-03') {
+    const url = `${apiUrl}/schedule?country=${country}&date=${date}`;
+    const response = await fetch(url);
+    if (response.ok) {
         return await response.json();
-    }else {
-        new Error(`Error: ${response.statusText}`);
     }
+    new Error(`Error: ${response.statusText}`);
 }
 
-function getHour(arr) {
-    const secondArray = [];
+function filterSchedule(arr) {
+    const filteredSchedule = [];
     arr.filter(element => {
-        element.airtime.match(regex) ? secondArray.push(element) : element;
+        if (isProperCategoryFilms(element) && isProperHour(element)) filteredSchedule.push(element);
     })
-    getCategoryFilms(secondArray)
+    displayFilms(filteredSchedule);
 }
 
-function getCategoryFilms(data) {
-    const newArray = [];
-    data.filter(element => {
-        for(let el of element.show.genres) {
-            if(el.match(regexCat)) {
-                newArray.push(element);
-                return newArray;
-            }
+function isProperHour(program) {
+    return program.airtime.match(regex)
+}
+
+function isProperCategoryFilms(program) {
+    for (let genre of program.show.genres) {
+        if (genre.match(regexCat)) {
+            return true;
         }
-    })
-    displayFilms(newArray);
+    }
+    return false;
 }
 
 function displayFilms(arr) {
-    for(let element of arr) {
+    for (let element of arr) {
         const info = document.createElement("div");
-        let tags;
-        element.show.genres ? tags = element.show.genres.join('#') : tags = " ";
+        let tags = element.show.genres;
+        tags.map((genre, index) => {
+            tags[index] = `#${genre}`;
+        });
+
+        tags = tags ? tags.join(', ') : " ";
         let countryZone;
-        element.show.webChannel ? countryZone=element.show.webChannel.country.timezone : countryZone = "";
+        element.show.webChannel ? countryZone = element.show.webChannel.country.timezone : countryZone = "";
         info.innerText = `${element.show.language} ${element.name} ${element.show.name} ${tags} ${element.airdate} ${element.airtime} ${countryZone}`;
         body.appendChild(info);
         const image = document.createElement("img");
